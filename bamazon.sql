e6d74c79249c6779be501f10f1452c95f768c3ab
Joseph <joseph@ahnga.net>
2019-02-03 20:59:45 -0800

    Completed challenge #1

--- bamazon.sql
+++ bamazon.sql
@@ -4,40 +4,40 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
-    product_name TEXT NOT NULL,
-    department_name TEXT NOT NULL,
-    price FLOAT(10,2) DEFAULT 0,
-    stock_quantity INT DEFAULT 0,
-    PRIMARY KEY (item_id)
+    Item_ID INT NOT NULL AUTO_INCREMENT,
+    Product_Name TEXT NOT NULL,
+    Department_Name TEXT NOT NULL,
+    Price FLOAT(10,2) DEFAULT 0,
+    Stock_Quantity INT DEFAULT 0,
+    PRIMARY KEY (Item_ID)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Keyboard", "Computer", 39.50, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Mouse", "Computer", 24.50, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Monitor", "Computer", 300, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Notebook", "Computer", 1000, 5);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Shirt", "Clothes", 29.99, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Pants", "Clothes", 39.99, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Jacket", "Clothes", 80, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Elec Drum Set", "Instruments", 2000, 2);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Acoustic Guitar", "Instruments", 800, 5);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
 VALUES ("Guitar Strings", "Instruments", 12.50, 20);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,16 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var CFonts = require("cfonts");
+
+var msg = "";
+var list = [];
+var product;
+var quantity;
+
+var red = "\x1b[37m\x1b[41m";
+var blue = "\x1b[37m\x1b[44m";
+var blink = "\x1b[5m";
+var reset = "\x1b[0m";
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,5 +25,122 @@ connection.connect(function (err) {
 });
 
 function start() {
+    console.clear();
+    console.log("\n\n");
+    CFonts.say(' BAMAZON', {
+        font: 'block',              // define the font face
+        align: 'left',              // define text alignment
+        colors: ['red'],            // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+    });
+    console.log("\n\n" + " ".repeat(32) + blink + "··· LOADING ···\n\n\n\n\n" + reset + " ".repeat(25) + "© Copyright 2018 Joseph Ahn");
 
+    setTimeout(displayInven, 3000);
+};
+
+function displayInven() {
+    connection.query(`SELECT * FROM products`, function (err, res) {
+        if (err) throw err;
+        list = res;
+        var transformed = list.reduce(function (acc, { Item_ID, ...x }) {
+            acc[Item_ID] = x;
+            return acc
+        }, {});
+        console.clear();
+        console.log("\n\n");
+        console.table(transformed);
+        console.log("\n" + msg + "\n");
+
+        enterItem();
+    });
+};
+
+function enterItem() {
+    inquirer.prompt([
+        {
+            name: "idx",
+            message: " ".repeat(13) + "Which product do you want? (Enter index#):"
+        }
+    ]).then(function (answer) {
+        product = -1;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].Item_ID === Number(answer.idx)) {
+                product = i;
+                break;
+            };
+        };
+        if (product === -1) {
+            msg = " ".repeat(7) + red + " No products found... Please enter the correct product idx# " + reset;
+            displayInven();
+        } else {
+            enterQty();
+        };
+    });
+};
+
+function enterQty() {
+    inquirer.prompt([
+        {
+            name: "qty",
+            message: " ".repeat(13) + "How many units do you want to purchase?  :"
+        }
+    ]).then(function (answer) {
+        if (answer.qty > list[product].Stock_Quantity) {
+            msg = " ".repeat(18) + red + " ...Insufficient quantity available... " + reset;
+            displayInven();
+        } else if (answer.qty <= 0) {
+            msg = " ".repeat(17) + red + " ...Please enter a positivie quantity... " + reset;
+            displayInven();
+        } else {
+            quantity = answer.qty;
+            update();
+        };
+    });
+};
+
+function update() {
+    connection.query(
+        "UPDATE products SET Stock_Quantity = ? WHERE Item_ID = ?",
+        [list[product].Stock_Quantity - quantity, list[product].Item_ID],
+        function (err, res) {
+            if (err) throw err;
+        });
+    checkOut();
+};
+
+function checkOut() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        list = res;
+        var transformed = list.reduce(function (acc, { Item_ID, ...x }) {
+            acc[Item_ID] = x;
+            return acc
+        }, {});
+        console.clear();
+        console.log("\n\n");
+        console.table(transformed);
+        console.log("\n" + " ".repeat(27) + blue + " Total price is $" + (list[product].Price * quantity).toFixed(2) + " " + reset + "\n");
+        tryAgain();
+    });
+};
+
+function tryAgain() {
+    inquirer.prompt([
+        {
+            name: "ask",
+            type: "list",
+            message: "Continue shopping?",
+            choices: ["YES / buy more", "NO / exit the app"]
+        }
+    ]).then(function (answer) {
+        if (answer.ask === "YES / buy more") {
+            displayInven();
+        } else {
+            connection.end();
+        };
+    });
 };
